e56b8fafad5b091a72aa38d22b66fba3fe905994
Lewis Daly <lewisdaly@me.com>
2019-07-23 10:06:02 +0800

    Playing with raml docs

---
+++ src/routes/healthCheck/healthCheck.routes.ts
@@ -0,0 +1,12 @@
+import koaRouter from 'koa-router'
+import { shallow } from './healthCheck.handler'
+
+/**
+ * Health check routes: used by load balancers to determine if traffic should
+ * be routed to nodes.
+ */
+const healthCheckRouter = new koaRouter()
+  .get('/', shallow)
+  // .get('/deep', deep);
+
+module.exports = { healthCheckRouter };
