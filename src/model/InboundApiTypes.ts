3caca33d923b6de108814ed665b11aa20edbe768
Lewis Daly <lewisdaly@me.com>
2019-08-23 16:03:40 +0800

    implement mock inbound api

---
+++ src/model/InboundApiTypes.ts
@@ -0,0 +1,122 @@
+
+export enum IdType {
+  MSISDN = 'MSISDN',
+  ACCOUNT_ID = 'ACCOUNT_ID',
+}
+
+export type IdValue = String
+
+export type ParticipantsResponse = {
+  fspId: String
+}
+
+export type TransferParty = {
+  idType: IdType,
+  idValue: IdValue,
+  displayName?: String,
+  firstName?: String,
+  middleName?: String,
+  lastName?: String,
+  dateOfBirth?: String, //Date of birth in the form YYYY-MM-DD.
+  merchantClassificationCode?: String,
+}
+
+//A Mojaloop API quote identifier(UUID).
+export type QuoteId = String
+
+//ID of the transaction, the ID is decided by the Payer FSP during the creation of the quote.
+export type TransactionId = String
+
+export type TransferId = String
+
+export enum AmountType {
+  SEND = 'SEND',
+  RECEIVE = 'RECEIVE',
+}
+
+export enum Currency {
+  // TODO: Add rest of currencies later on
+  AUD = "AUD",
+  USD = "USD",
+  XOF = "XOF",
+}
+
+export type Money = String;
+
+export enum TransactionType {
+  TRANSFER = 'TRANSFER',
+}
+
+export enum Initiator {
+  PAYER = 'PAYER',
+  PAYEE = 'PAYEE',
+}
+
+export enum InitiatorType {
+  CONSUMER = 'CONSUMER',
+  AGENT = 'AGENT',
+  BUSINESS = 'BUSINESS',
+  DEVICE = 'DEVICE',
+}
+
+export type GeoCode = {
+  latitude: String, //The API data type Latitude is a JSON String in a lexical format that is restricted by a regular expression for interoperability reasons.
+  longitude: String, //The API data type Longitude is a JSON String in a lexical format that is restricted by a regular expression for interoperability reasons.
+}
+
+export type Timestamp = String
+
+export type QuoteRequest = {
+  quoteId: QuoteId,
+  transactionId: TransactionId,
+  to: TransferParty,
+  from: TransferParty,
+  amountType: AmountType,
+  amount: Money,
+  currency: Currency,
+  feesAmount?: Money,
+  feesCurrency?: Currency,
+  transactionType: TransactionType,
+  initiator: Initiator,
+  initiatorType: InitiatorType,
+  geoCode?: GeoCode,
+  note?: String,
+  expiration?: Timestamp
+}
+
+export type QuoteResponse = {
+  quoteId: QuoteId,
+  transactionId: TransactionId,
+  transferAmount: Money,
+  transferAmountCurrency: Currency,
+  payeeReceiveAmount?: Money,
+  payeeReceiveAmountCurrency?: Currency,
+  payeeFspFeeAmount?: Money,
+  payeeFspFeeAmountCurrency?: Currency,
+  payeeFspCommissionAmount?: Money,
+  payeeFspCommissionAmountCurrency?: Currency,
+  expiration?: Timestamp,
+  geoCode?: GeoCode,
+}
+
+export type TransferRequest = {
+  transferId: TransferId,
+  quote: QuoteResponse,
+  from: TransferParty,
+  to: TransferParty,
+  amountType: AmountType,
+  currency: Currency,
+  amount: Money,
+  transactionType: TransactionType,
+  note?: String,
+}
+
+export type TransferResponse = {
+  homeTransactionId: String
+}
+
+export enum TransferStatus {
+  ERROR_OCCURRED = 'ERROR_OCCURRED',
+  WAITING_FOR_QUOTE_ACCEPTANCE = 'WAITING_FOR_QUOTE_ACCEPTANCE',
+  COMPLETED = 'COMPLETED',
+}
---
+++ src/routes/inbound/inbound.handler.ts
@@ -0,0 +1,58 @@
+import { Context } from 'koa'
+
+import { 
+  ParticipantsResponse, 
+  TransferParty, 
+  IdType, 
+  QuoteResponse, 
+  Currency, 
+  TransferResponse 
+} from '../../model/InboundApiTypes'
+
+export async function getParticipants(ctx: Context) {
+  //TODO: move validation logic elsewhere
+  /*
+    - check that idType is in IdType enum
+    - check that accountId is a string with a min length 1, max length 128
+  */
+
+  //Temp mock response
+  const response: ParticipantsResponse = {
+    fspId: 'glasper'
+  }
+
+  ctx.body = response
+}
+
+export async function getParties(ctx: Context) {
+  //Temp mock response
+  const response: TransferParty = {
+    idType: IdType.MSISDN,
+    idValue: "+61404404404"
+  }
+
+  ctx.body = response;
+}
+
+export async function postQuoteRequests(ctx: Context) {
+  //TODO: parse the quote request with a fixed rate...
+
+  //Temp mock response
+  const response: QuoteResponse = {
+    quoteId: '0000-0000-0000-0000',
+    transactionId: '0000-0000-0000-0000',
+    transferAmount: '100.00',
+    transferAmountCurrency: Currency.AUD,
+    //TODO: other fields?
+  }
+
+  ctx.body = response
+}
+
+export async function postTransfers(ctx: Context) {
+
+  const response: TransferResponse = {
+    homeTransactionId: '0000-0000-0000-0000',
+  }
+  ctx.body = response
+}
