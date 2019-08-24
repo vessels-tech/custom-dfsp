8d6f2910e1123432f09c134760e634dcf5ae77ac
Lewis Daly <lewisdaly@me.com>
2019-08-24 17:58:46 +0800

    trying to get getParties to work with als

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 const config = {
   "HOSTNAME": "http://localhost",
   "PORT": 4999,
-  // "DFSP_ID": "lewbank1",
-  "DFSP_ID": "mojaloop-sdk",
+  "DFSP_ID": "lewbank1",
+  // "DFSP_ID": "mojaloop-sdk",
   // "PEER_ENDPOINT": "localhost:3000",
   // "PEER_ENDPOINT": "ml-api-adapter.moja-box.vessels.tech",
   "PEER_ENDPOINT": "localhost:4000",
--- src/routes/adapter/adapter.routes.ts
+++ src/routes/adapter/adapter.routes.ts
@@ -4,7 +4,7 @@ import KoaRouter from 'koa-router'
 import { shallow } from '../healthCheck/healthCheck.handler'
 import {
   getParticipants,
-  // getParties,
+  getParties,
   postQuoteRequests,
   postTransfers,
 } from './adapter.handler'
@@ -15,8 +15,8 @@ import {
 const router = new KoaRouter()
 
 router.get('/health', shallow);
-router.get('/participants/:idType/:idValue', getParticipants)
-// router.get('/parties/:idType/:idValue', getParties)
+router.get('/participants/:idType/:idValue', getParticipants) //I don't think we will use getParticipants, but getParties instead
+router.get('/parties/:idType/:idValue', getParties)
 router.post('/quoterequests', postQuoteRequests)
 router.post('/transfers', postTransfers)
 
