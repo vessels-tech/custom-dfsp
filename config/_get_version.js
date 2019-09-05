6f9eb910bb26632af71d26cbabd3ac97d087ebf0
Lewis Daly <lewisdaly@me.com>
2019-09-06 00:05:04 +0800

    debugging missing callbacks

--- config/_get_version.js
+++ config/_get_version.js
@@ -2,5 +2,8 @@
 
 /* A command line utility to easily get the version of the package for Make */
 
-const {version} = require('../package.json')
-console.log(version)
+// const {version} = require('../package.json')
+// console.log(version)
+
+//Temporary override for bad internet - don't use package.json
+console.log('0.1.6')
